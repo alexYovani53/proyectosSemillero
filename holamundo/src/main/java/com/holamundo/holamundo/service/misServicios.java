dab435dd88755cbe6f7fbf80d11dbd627a3341db
alexYovani53 <alexyovani53@gmail.com>
2021-12-02 22:54:01 -0600

    HOLA MUNDO CON SPRING

---
+++ holamundo/src/main/java/com/holamundo/holamundo/service/misServicios.java
@@ -0,0 +1,18 @@
+package com.holamundo.holamundo.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/miServicio")
+public class misServicios {
+	@GetMapping(path="/holaMundo/{nombre}")
+	
+	public String holaMundo(@PathVariable("nombre") String nombre) {
+		return "Bienvenido! " + nombre + ". Este es mi primer paso con SpringBoot";
+	}
+	
+	
+}
